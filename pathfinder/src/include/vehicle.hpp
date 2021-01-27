4428e0967eab6975b40391e6f0255849508fc3a4
Globins <50190118+Globins@users.noreply.github.com>
2021-01-27 13:27:27 -0800

    Reorganized Main to better match the architecture of the server/client relationship

--- pathfinder/src/include/vehicle.hpp
+++ pathfinder/src/include/vehicle.hpp
@@ -11,6 +11,7 @@ public:
     float maxTurnAngle;
     float turnRadius = .6;
     float velocity = 1;
+    vector<VehicleState> current_path;
     Vehicle(float width, float length);
     VehicleState getNextState(VehicleState current, Steer steer, Gear gear, float delta_time);
 };
--- pathfinder/src/main.cpp
+++ pathfinder/src/main.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <map>
 //grid measurements of 10x10 c
 int main()
 {
@@ -19,17 +20,55 @@ int main()
     grid.addObstacle(32,25);
     grid.addObstacle(31,25);
     grid.addObstacle(30,25);
-    Vehicle testcar = Vehicle(1, 1);
     HybridAStar pathfinder = HybridAStar(&grid);
-    VehicleState start = VehicleState{35, 5, deg2rad(90), Forward, Straight};
-    VehicleState end = VehicleState{4, 31, deg2rad(180), Forward, Straight};
-    vector<VehicleState> path = pathfinder.run(start, end, testcar, true);
-    ofstream myfile;
-    myfile.open("output.txt");
-    cout << "TEST" << endl;
-    for(VehicleState state : path)
-    {
-        myfile << "(" << state.posX << ", " << state.posY << ", " << state.ori << ", " << state.steer << ", " << state.gear << ")\n";
-    }
-    myfile.close();
+    //Neural Network training will go here
+    //it will run randomly positioned car positions of varied amount of cars in a random map for 10k times
+    map<int, Vehicle*> vehicleIDMap;
+
+
+    // PATH WILL BE (X, Y, ORI, GEAR, STEER, TIMEATNODE)
+    
+    // while(1)
+    // {
+        //SERVER: Will run while loop that will take requests in form of:
+        // VehicleID ObstUpdate [coords relative to current position]
+        // VehicleID (StartPos) (EndPos)
+        int vehicleID = 0;
+        bool obstRequest = false;
+        if(obstRequest)
+        {
+            //Find obst positon relative to current
+        }
+        else
+        {
+            VehicleState start = VehicleState{35, 5, deg2rad(90), Forward, Straight};
+            VehicleState end = VehicleState{4, 31, deg2rad(180), Forward, Straight};
+            if(!vehicleIDMap.count(vehicleID))
+            {
+                vehicleIDMap[vehicleID] = new Vehicle(1, 1);
+            }
+            vehicleIDMap[vehicleID]->current_path = pathfinder.run(start, end, *vehicleIDMap[vehicleID], true);
+            //COMPARE ALL PATHS IN IDMAP
+            bool collision = false;
+
+            if(collision)
+            {
+                //RUN ANN
+                //SEND PATHS TO VEHICLES AFFECTED
+            }
+            else
+            {
+                // SERVER: WILL SEND PATH TO VEHICLE FORM OF:
+                // X Y ORI GEAR STEER
+                ofstream myfile;
+                myfile.open("output.txt");
+                cout << "TEST" << endl;
+                for(VehicleState state : vehicleIDMap[vehicleID]->current_path)
+                {
+                    myfile << "(" << state.posX << ", " << state.posY << ", " << state.ori << ", " << state.steer << ", " << state.gear << ")\n";
+                }
+                myfile.close();
+            }
+        }
+    // }
 }
